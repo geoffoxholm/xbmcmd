9400bb752ba7653aecc206eb2418cf329faaa38d
Geoff <geoff.oxholm@gmail.com>
2013-10-01 09:34:45 -0400

    Added a bunch more commands (play, stop, pause, terminate, scan, clean) and some optimizations)

--- xbmcmd.py
+++ xbmcmd.py
@@ -2,27 +2,48 @@ import cmd
 import json
 from urllib.request import urlopen, Request
 import sys
+import logging as log
 
 HOST = "localhost"
 PORT = "8080"
+VERBOSE = True
+
+verbose_print = None
 
 class XBMCMD(cmd.Cmd):
     HEADERS = {'content-type' : 'application/json'}
     COMMANDS = {"movies" : "VideoLibrary.GetMovies",
-                "detail": "VideoLibrary.GetMovieDetails"}
-    PARAMS = {"detail"  : {"movieid" : int}}
+                "detail": "VideoLibrary.GetMovieDetails",
+                "scan"  : "VideoLibrary.Scan",
+                "clean" : "VideoLibrary.Clean",
+                "terminate"  : "Application.Quit",
+                "play" : "Player.Open",
+                "stop" : "Player.Stop",
+                "pause" : "Player.PlayPause"}
+
+    PARAMS = {"detail"  : {"movieid" : int},
+              "play" : {"item" : {"movieid" : int}}}
     HARD_PARAMS   = {"movies"   : {"properties" : ["year"]},
-                     "detail" : {"properties" : ["trailer", "year", "streamdetails", "file", "imdbnumber", "dateadded"] }}
+                     "detail" : {"properties" : ["trailer", "year", "streamdetails", "file", "imdbnumber", "dateadded"] },
+                     "stop" : {"playerid" : 1},
+                     "pause" : {"playerid" : 1}}
     prompt = "XBMC> "
     intro  = "Type `?` or `help` for command list"
-
 
     def __init__(self, *args, URL = "http://%s:%s/jsonrpc" % (HOST, PORT)):
         """Constructor, optional argument is the jsonrpc URL"""
         self.URL = URL
-        result = self.send_request("movies")
-        self.movies = {movie["label"] : movie["movieid"] for movie in result["result"]["movies"]}
+        self._movies = None # Cached movie list
         super().__init__()
+
+    @property
+    def movies(self):
+        """Cached list of movie names and ids from database"""
+        if self._movies is None:
+            log.info("Querying list of movies")
+            result = self.send_request("movies")
+            self._movies =  {movie["label"] : movie["movieid"] for movie in result["result"]["movies"]}
+        return self._movies
 
     def send_request(self, command, args = []):
         """Packages the command, and args into a JSON request for XBMC"""
@@ -35,7 +56,7 @@ class XBMCMD(cmd.Cmd):
                 if type(style) == dict:
                     value = {}
                     for (extra_2, style_2) in style.items():
-                        value_2 = sys.argv[index]
+                        value_2 = args[index]
                         value[extra_2] = style_2(value_2)
                         index += 1
                     index -= 1
@@ -56,6 +77,8 @@ class XBMCMD(cmd.Cmd):
         if params is not None:
             data["params"] = params
 
+        log.info(data)
+
         request = Request(self.URL, headers = XBMCMD.HEADERS, data = json.dumps(data).encode('utf-8'))
         handler = urlopen(request)
         return json.loads(handler.read().decode("utf-8"))
@@ -75,10 +98,8 @@ class XBMCMD(cmd.Cmd):
         return self.get_movie_names(*args)
 
     def get_id(self, line):
-        if line in self.movies:
-            return self.movies[line]
         try:
-            if int(line) in self.movies.values():
+            if int(line):
                 return int(line)
             else:
                 return None
@@ -89,16 +110,67 @@ class XBMCMD(cmd.Cmd):
             else:
                 return None
 
+    @staticmethod
+    def check_result(result):
+        if "result" in result:
+            if result["result"] == "OK":
+                log.info(result)
+                return True
+            else:
+                log.error(result["result"])
+                log.info(result)
+                return False
+        else:
+            if "error" in result:
+                log.error(result["error"]["message"])
+            else:
+                log.error("Failed to execute command")
+            log.info(result)
+            return False
+
     #
     # -- Commands ----
     #
 
-    def do_quit(self, args):
+    def default(self, line):
+        try:
+            check_result(self.send_request(line))
+        except:
+            log.error("Unknown syntax:", line)
+
+    def do_terminate(self, line):
+        """Stops XBMC"""
+        XBMCMD.check_result(self.send_request("terminate"))
+
+    def do_scan(self, line):
+        """Initiates a scan of your library (in the background)"""
+        XBMCMD.check_result(self.send_request("scan"))
+
+    def do_clean(self, line):
+        """Initiates a clean of your library (in the background). May take a long time to complete."""
+        XBMCMD.check_result(self.send_request("clean"))
+
+    def do_stop(self, line):
+        """Stops the currently playing media"""
+        XBMCMD.check_result(self.send_request("stop"))
+
+    def do_pause(self, line):
+        """Toggles pause for the currently playing media"""
+        XBMCMD.check_result(self.send_request("pause"))
+
+    def do_quit(self, line):
         """Quits"""
         print()
         return True
     do_EOF = do_quit # Enable Ctrl-d
 
+    def do_play(self, line):
+        """Starts playing the movie requested"""
+        movie_id = self.get_id(line)
+        if movie_id is None:
+            log.error("You must pass a valid movie_id or a (prefix of a) movie title")
+            return
+        XBMCMD.check_result(self.send_request("play", [movie_id]))
 
     def do_movies(self, line):
         """List all movies (no arguments)"""
@@ -110,7 +182,7 @@ class XBMCMD(cmd.Cmd):
         """Outputs detail about a given movie. Pass the ID of the movie or (a prefix of) its title."""
         movie_id = self.get_id(line)
         if movie_id is None:
-            print("You must pass a valid movie_id or a (prefix of a) movie title")
+            log.error("You must pass a valid movie_id or a (prefix of a) movie title")
             return
         result = self.send_request("detail", [movie_id])
         details = result["result"]["moviedetails"]
@@ -133,6 +205,8 @@ class XBMCMD(cmd.Cmd):
 
 
 if __name__ == '__main__':
+    log.basicConfig(format="%(levelname)s: %(message)s", level=log.ERROR)
+
     XBMC = XBMCMD()
 
     if len(sys.argv) > 1:
